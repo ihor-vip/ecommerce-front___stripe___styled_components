fe9f6b9c63bdbf70dd3d03340054dcfd58698e6b
ihor-vip <vip20140902@gmail.com>
2023-05-08 14:59:08 +0300

    create feature product component

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,24 @@
 import Header from "@/components/Header";
+import Featured from "@/components/Featured";
+import {Product} from "@/models/Product";
+import {mongooseConnect} from "@/lib/mongoose";
 
-export default function Home() {
+export default function HomePage({product}) {
   return (
     <div>
-      <Header />
+        <Header />
+        <Featured  product={product}/>
     </div>
   )
+}
+
+export async function getServerSideProps() {
+    const featuredProductId = '6455f61d9a61072fbc683ff8';
+    await mongooseConnect();
+    const product = await Product.findById(featuredProductId);
+    return {
+        props: {
+            product: JSON.parse(JSON.stringify(product)),
+        },
+    };
 }
