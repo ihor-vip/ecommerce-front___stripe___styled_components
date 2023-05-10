7d6f1d91843957d981312325cc0fccc58a1bc1e3
ihor-vip <vip20140902@gmail.com>
2023-05-10 17:01:21 +0300

    order product

--- pages/cart.js
+++ pages/cart.js
@@ -51,7 +51,7 @@ const CityHolder = styled.div`
 `;
 
 export default function CartPage() {
-    const {cartProducts, addProduct, removeProduct} = useContext(CartContext);
+    const {cartProducts, addProduct, removeProduct, clearCart} = useContext(CartContext);
     const [products,setProducts] = useState([]);
     const [name,setName] = useState('');
     const [email,setEmail] = useState('');
@@ -59,6 +59,7 @@ export default function CartPage() {
     const [postalCode,setPostalCode] = useState('');
     const [streetAddress,setStreetAddress] = useState('');
     const [country,setCountry] = useState('');
+    const [isSuccess,setIsSuccess] = useState(false);
 
     useEffect(() => {
         if (cartProducts.length > 0) {
@@ -70,6 +71,16 @@ export default function CartPage() {
             setProducts([]);
         }
     }, [cartProducts]);
+
+    useEffect(() => {
+        if (typeof window === 'undefined') {
+            return;
+        }
+        if (window?.location.href.includes('success')) {
+            setIsSuccess(true);
+            clearCart();
+        }
+    }, []);
 
     function moreOfThisProduct(id) {
         addProduct(id);
@@ -94,7 +105,7 @@ export default function CartPage() {
         total += price;
     }
 
-    if (window.location.href.includes('success')) {
+    if (isSuccess) {
         return (
             <>
                 <Header />
