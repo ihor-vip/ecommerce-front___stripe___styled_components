584f8bbb23c570c11d2183d1343567e66b141fdb
ihor-vip <vip20140902@gmail.com>
2023-05-11 12:06:04 +0300

    create all products page

--- components/NewProducts.js
+++ components/NewProducts.js
@@ -1,12 +1,7 @@
 import styled from "styled-components";
 import Center from "@/components/Center";
-import ProductBox from "@/components/ProductBox";
+import ProductsGrid from "@/components/ProductsGrid";
 
-const ProductsGrid = styled.div`
-  display: grid;
-  grid-template-columns: 1fr 1fr 1fr 1fr;
-  gap: 20px;
-`;
 
 const Title =  styled.h2`
   font-size: 2rem;
@@ -18,11 +13,7 @@ export default function NewProducts({products}) {
     return (
         <Center>
             <Title>New Arrivals</Title>
-            <ProductsGrid>
-                {products?.length > 0 && products.map(product => (
-                    <ProductBox {...product} />
-                ))}
-            </ProductsGrid>
+            <ProductsGrid products={products} />
         </Center>
     )
 }
