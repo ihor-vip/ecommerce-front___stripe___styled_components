fc19329ecca8ec7e412edb7d643f89f628e049b0
ihor-vip <vip20140902@gmail.com>
2023-05-12 09:27:06 +0300

    make it mobile responsive app

--- components/ProductBox.js
+++ components/ProductBox.js
@@ -1,8 +1,7 @@
+import {useContext} from "react";
+import Link from "next/link";
 import styled from "styled-components";
 import Button from "@/components/Button";
-import CartIcon from "@/components/icons/CartIcon";
-import Link from "next/link";
-import {useContext} from "react";
 import {CartContext} from "@/components/CartContext";
 
 const ProductWrapper = styled.div``;
@@ -36,20 +35,29 @@ const ProductInfoBox = styled.div`
 
 const PriceRow = styled.div`
   display: block;
+  @media screen and (min-width: 768px) {
+    display: flex;
+    gap: 5px;
+  }
   align-items: center;
-  justify-content:space-between;
-  margin-top:2px;
+  justify-content: space-between;
+  margin-top: 2px;
 `;
 
 const Price = styled.div`
   font-size: 1rem;
-  font-weight:400;
+  font-weight: 400;
   text-align: right;
+  @media screen and (min-width: 768px) {
+    font-size: 1.2rem;
+    font-weight: 600;
+    text-align: left;
+  }
 `;
 
 export default function ProductBox({_id,title,description,price,images}) {
     const {addProduct} = useContext(CartContext);
-    const url = '/product'+_id;
+    const url = '/product/'+_id;
     return (
         <ProductWrapper>
             <WhiteBox href={url}>
--- components/ProductsGrid.js
+++ components/ProductsGrid.js
@@ -5,6 +5,9 @@ const StyledProductsGrid = styled.div`
   display: grid;
   grid-template-columns: 1fr 1fr;
   gap: 20px;
+  @media screen and (min-width: 768px) {
+    grid-template-columns: 1fr 1fr 1fr 1fr;
+  }
 `;
 
 export default function ProductsGrid({products}) {
